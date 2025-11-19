c671e8d1b8fa96d750e04c01947257d14d267da6
heygideon <47083178+heygideon@users.noreply.github.com>
2025-11-19 19:24:41 +0000

    feat: randomise neocat on homescreen

---
+++ src/lib/assets/neocat/index.ts
@@ -0,0 +1,9 @@
+import aww from './aww.png';
+import cuteReach from './cute-reach.png';
+import heart from './heart.png';
+import laptop from './laptop.png';
+import pleadingReach from './pleading-reach.png';
+import snuggle from './snuggle.png';
+import winkBlep from './wink-blep.png';
+
+export default [aww, cuteReach, heart, laptop, pleadingReach, snuggle, winkBlep];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-	import neocat from '$lib/assets/neocat/heart.png';
 	import rocket from '$lib/assets/images/rocket.png';
 	import smile from '$lib/assets/images/smile.png';
 	import globe from '$lib/assets/images/globe.png';
 	import message from '$lib/assets/images/message.png';
 
 	import Taskbar from '$lib/components/taskbar/Taskbar.svelte';
+
+	import neocats from '$lib/assets/neocat';
+
+	let catIdx = $state(Math.floor(Math.random() * neocats.length));
+	let neocat = $derived(neocats[catIdx]);
 </script>
 
 <div class="pointer-events-none fixed inset-x-0 top-0 flex items-center justify-center gap-1.5 p-4">
@@ -28,31 +32,38 @@
 				<span class="font-semibold">Hey, I’m Gideon,</span><br />
 				and I make things.
 			</h1>
-			<img src={neocat} alt="" class="-mt-4 -ml-2 size-32 rotate-12 drop-shadow-md" />
+			<button
+				onclick={() => {
+					catIdx = (catIdx + 1) % neocats.length;
+				}}
+				class="-mt-4 -ml-2 block size-fit rotate-12 transition hover:scale-110 hover:rotate-6 active:scale-95 active:opacity-75"
+			>
+				<img src={neocat} alt="" class="size-32 drop-shadow-md" />
+			</button>
 		</div>
 		<div class="mt-6 flex gap-4">
 			<button
-				class="flex w-20 flex-col items-center rounded-sm border border-transparent pb-0.5 text-center hover:border-stone-300/50 hover:bg-white/20"
+				class="flex w-20 flex-col items-center rounded-sm border border-transparent pb-0.5 text-center hover:border-stone-300/50 hover:bg-white/20 focus:border-stone-300 focus:bg-white/50"
 			>
-				<img src={rocket} alt="" />
+				<img src={rocket} alt="" class="h-12" />
 				<span class="mt-1 text-sm">Projects</span>
 			</button>
 			<button
-				class="flex w-20 flex-col items-center rounded-sm border border-transparent pb-0.5 text-center hover:border-stone-300/50 hover:bg-white/20"
+				class="flex w-20 flex-col items-center rounded-sm border border-transparent pb-0.5 text-center hover:border-stone-300/50 hover:bg-white/20 focus:border-stone-300 focus:bg-white/50"
 			>
-				<img src={smile} alt="" />
+				<img src={smile} alt="" class="h-12" />
 				<span class="mt-1 text-sm">About me</span>
 			</button>
 			<button
-				class="flex w-20 flex-col items-center rounded-sm border border-transparent pb-0.5 text-center hover:border-stone-300/50 hover:bg-white/20"
+				class="flex w-20 flex-col items-center rounded-sm border border-transparent pb-0.5 text-center hover:border-stone-300/50 hover:bg-white/20 focus:border-stone-300 focus:bg-white/50"
 			>
-				<img src={globe} alt="" />
+				<img src={globe} alt="" class="h-12" />
 				<span class="mt-1 text-sm">Friends</span>
 			</button>
 			<button
-				class="flex w-20 flex-col items-center rounded-sm border border-transparent pb-0.5 text-center hover:border-stone-300/50 hover:bg-white/20"
+				class="flex w-20 flex-col items-center rounded-sm border border-transparent pb-0.5 text-center hover:border-stone-300/50 hover:bg-white/20 focus:border-stone-300 focus:bg-white/50"
 			>
-				<img src={message} alt="" />
+				<img src={message} alt="" class="h-12" />
 				<span class="mt-1 text-sm">Say hi!</span>
 			</button>
 		</div>
